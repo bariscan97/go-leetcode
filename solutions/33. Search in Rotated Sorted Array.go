4a2b04d89242e24bc95d150397e9e197c3a15003
Barış karakuş <bariscan9788@gmail.com>
2025-07-29 17:41:17 +0300

    solution

---
+++ solutions/33. Search in Rotated Sorted Array.go
@@ -0,0 +1,26 @@
+func search(nums []int, target int) int {
+    low, high := 0, len(nums) - 1
+
+    for low <= high {
+        mid := (low + high) / 2
+
+        if nums[mid] == target {
+            return mid
+        }
+
+        if nums[low] <= nums[mid] {
+            if nums[low] <= target && target < nums[mid] {
+                high = mid - 1
+            } else {
+                low = mid + 1
+            }
+        } else {
+            if nums[mid] < target && target <= nums[high] {
+                low = mid + 1
+            } else {
+                high = mid - 1
+            }
+        }
+    }
+    return -1
+}
